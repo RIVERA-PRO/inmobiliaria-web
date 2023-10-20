3b4c14c6f9536f7a5583ce572cbd4446c684e39f
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-10-20 15:16:42 -0300

    home

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Hero.css'
+import InputSearchHome from '../InputSearchHome/InputSearchHome'
 export default function Hero() {
     return (
         <div className='heroContain'>
 
+            <h1>Encontrá tu próximo hogar</h1>
+            <InputSearchHome />
+
+            <div className='carters'>
+                <span>Propiedades</span>
+                <span>Alquiler</span>
+                <span>Cabañas</span>
+                <span>Mucho más</span>
+            </div>
 
         </div>
     )
